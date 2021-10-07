86e349ce172b7d7844583f4ed7297f4a9b0689b1
lfgaray0331 <74473239+lfgaray0331@users.noreply.github.com>
2021-10-07 00:37:11 -0500

    Sniffer.js
    Update

--- back/sniffer.js
+++ back/sniffer.js
@@ -6,6 +6,11 @@ const cors = require("cors");
 server.use(cors());
 server.use(express.json());
 
+let last = {
+  ti: 0,
+  tf: 0,
+};
+
 const connection = mysql.createConnection(db);
 
 server.listen(4000, () => console.log("Server on port 4000"));
@@ -29,14 +34,30 @@ sniffer.on("message", async (msg, _rinfo) => {
   let num_longitud = longitud.split("=")[1];
   let fecha1 = mes.split(";")[2];
   let fecha = fecha1.split("=")[1];
-  const query = `INSERT INTO Locations (latitude,longitude,fecha) values (${num_latitud},${num_longitud},${fecha})`;
+
+  let query = `select * from Locations`;
+
   connection.query(query, function (err, results) {
     if (err) {
       console.log(err.stack);
     }
 
     if (results) {
-      console.log("done");
+      if (
+        results[results.length - 1].latitude != num_latitud ||
+        results[results.length - 1].longitude != num_longitud
+      ) {
+        query = `INSERT INTO Locations (latitude,longitude,fecha) values (${num_latitud},${num_longitud},${fecha})`;
+        connection.query(query, function (error, res) {
+          if (error) {
+            console.log(error.stack);
+          }
+
+          if (res) {
+            console.log("done");
+          }
+        });
+      }
     }
   });
 });
@@ -58,9 +79,12 @@ server.get("/mensaje", (_req, res) => {
 });
 
 server.post("/history", (req, res) => {
-  console.log(req.body);
-
   const { ti, tf } = req.body;
+
+  last = {
+    ti,
+    tf,
+  };
 
   const query = `select * from Locations where fecha >= ${ti} and fecha <= ${tf}`;
 
@@ -77,4 +101,23 @@ server.post("/history", (req, res) => {
   });
 });
 
+server.post("/interval", (req, res) => {
+  const { r, lat_c, lng_c } = req.body;
+
+  const query = `select * from Locations where fecha >= ${last.ti} and fecha <= ${last.tf} and (latitude - (${lat_c}))*(latitude - (${lat_c})) + (longitude - (${lng_c}))*(longitude - (${lng_c})) < ${r}`;
+
+  connection.query(query, function (err, results) {
+    if (err) {
+      console.log(err.stack);
+    }
+
+    if (results) {
+      return res.json({
+        response: results,
+      });
+    }
+  });
+});
+
 sniffer.bind(5000, () => console.log("udp up on 5000"));
+
