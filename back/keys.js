7436631b99662897b83e51eb445ea98f3f6f6268
Laura Marcela Arango Correa <90224938+lmarangoc@users.noreply.github.com>
2021-10-07 00:41:16 -0500

    Delete back directory

--- back/keys.js
+++
@@ -1,9 +0,0 @@
-const db = {
-  host: "database-1.cmlwdskqtfdl.us-east-2.rds.amazonaws.com",
-  port: "3306",
-  user: "admin",
-  password: "L200107325a",
-  database: "data",
-};
-
-module.exports = { db };
