2adee96f3d0822618d2f6b0c4ee2bb97a36831b8
lfgaray0331 <74473239+lfgaray0331@users.noreply.github.com>
2021-10-07 00:39:27 -0500

    CustomMap.ja
    Update

--- front/src/components/CustomMap.js
+++ front/src/components/CustomMap.js
@@ -4,34 +4,70 @@ import {
   Marker,
   Popup,
   Polyline,
+  Circle,
 } from "react-leaflet";
 import L from "leaflet";
+import { useRef, useEffect, useState } from "react";
 
 const icon = L.icon({
   iconUrl: "/images/marker-icon.png",
   iconAnchor: L.point(12, 40),
 });
 
-function CustomMap({ data, poly, center, width }) {
+function CustomMap({ data, poly, center, width, onClick, canClick, canMove }) {
+  const map = useRef(null);
+
+  const [circle, setCircle] = useState({
+    lat: 0,
+    lng: 0,
+  });
+
+  useEffect(() => {
+    map.current?.on("click", (e) => {
+      setCircle(e.latlng);
+      onClick(e);
+    });
+  }, []);
+
+  useEffect(() => {
+    canMove && map.current?.setView(center, 10, {});
+  }, [center]);
+
   return (
-    <MapContainer style={{ width, height: "94vh" }} center={center} zoom={13}>
+    <MapContainer
+      style={{ width, height: "94vh" }}
+      center={center}
+      zoom={13}
+      whenReady={(e) => {
+        map.current = e.target;
+        console.log(e.target);
+      }}
+    >
       <TileLayer
         attribution='&copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      {data && (
-        <Marker position={data} icon={icon}>
-          <Popup>
-            <h1>{"Coordenadas"}</h1>
-            <p>Latitud:{data[0]}</p>
-            <p>Longitud:{data[1]}</p>
-          </Popup>
-        </Marker>
-      )}
+      {data &&
+        data[0] &&
+        data.map((d, i) => (
+          <Marker position={d.pos} icon={icon} key={i}>
+            <Popup>
+              <h1>{"Coordenadas"}</h1>
+              <p>Latitud: {d.pos[0]}</p>
+              <p>Longitud: {d.pos[1]}</p>
+              <p>Fecha: {new Date(d.date).toLocaleString()}</p>
+            </Popup>
+          </Marker>
+        ))}
 
-      <Polyline positions={poly} />
+      {poly && poly[0] && <Polyline positions={poly} />}
+
+      {canClick && circle.lat !== 0 && circle.lng !== 0 && (
+        <Circle center={circle} fillColor="red" radius={1200} />
+      )}
     </MapContainer>
   );
 }
 
 export default CustomMap;
+
