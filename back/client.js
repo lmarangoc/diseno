dd46f539e44eaba102ec1660a0639fc5f6c0a240
Laura Marcela Arango Correa <90224938+lmarangoc@users.noreply.github.com>
2021-10-21 00:18:32 -0500

    Add files via upload

---
+++ back/client.js
@@ -0,0 +1,36 @@
+const udp = require("dgram");
+
+// creating a client socket
+let client = udp.createSocket("udp4");
+
+client.on("message", function (msg, info) {
+  console.log("Data received from server : " + msg.toString());
+  console.log(
+    "Received %d bytes from %s:%d\n",
+    msg.length,
+    info.address,
+    info.port
+  );
+});
+
+const dlat = 0.01;
+const dlng = 0.01;
+let lat = 10.92;
+let lng = -74.77;
+
+const id = process.argv[2];
+const dir = process.argv[3];
+
+setInterval(() => {
+  //sending msg
+  const msg = `Latitude=${lat};Longitude=${lng};Fecha=${new Date().getTime()};Conductor=${id}`;
+  client.send(msg, 5000, "localhost", function (error) {
+    if (error) {
+      client.close();
+    } else {
+      console.log(msg);
+    }
+  });
+  lat += dir === "r" ? dlat : -dlat;
+  lng += dir === "r" ? dlng : -dlng;
+}, 3000);
