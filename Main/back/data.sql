4ce30adca302405444c6dee8ffd8140efb4bad82
Laura Marcela Arango Correa <90224938+lmarangoc@users.noreply.github.com>
2021-10-21 00:17:44 -0500

    Add files via upload

---
+++ Main/back/data.sql
@@ -0,0 +1,9 @@
+-- mysql -h database-1.cmlwdskqtfdl.us-east-2.rds.amazonaws.com -P 3306 -u admin -p
+
+CREATE TABLE Locations (
+    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    latitude DECIMAL(10,7),
+    longitude DECIMAL(10,7),
+    fecha BIGINT,
+    conductor BIGINT
+);
