d83ecc46a2d93ad7420b559b5947ef7c58ac81f3
Laura Marcela Arango Correa <90224938+lmarangoc@users.noreply.github.com>
2021-11-03 23:08:46 -0500

    Add files via upload

---
+++ back/keys.js
@@ -0,0 +1,9 @@
+const db = {
+  host: "database-1.cmlwdskqtfdl.us-east-2.rds.amazonaws.com",
+  port: "3306",
+  user: "admin",
+  password: "L200107325a",
+  database: "data",
+};
+
+module.exports = { db };
