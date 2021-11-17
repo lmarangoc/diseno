b03fe3a588fb98a5ecac81eaccafe9b7714e6fcc
briantuninorte <89306508+briantuninorte@users.noreply.github.com>
2021-11-16 22:43:21 -0500

    Update server.js

--- front/server.js
+++ front/server.js
@@ -9,5 +9,8 @@ app.get("/", function (_req, res) {
   res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
+app.get("/history", function (_req, res) {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
 
 app.listen(3000, () => console.log("front up"));
